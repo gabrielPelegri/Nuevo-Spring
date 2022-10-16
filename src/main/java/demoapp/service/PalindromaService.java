460eb89360615a5f9cf9b06e3ae47d8b97b9d67c
Gabriel Pelegri <gpelerii@myuax.com>
2022-10-16 13:11:52 +0200

    Reconocer si la palabra es una única letra

--- src/main/java/demoapp/service/PalindromaService.java
+++ src/main/java/demoapp/service/PalindromaService.java
@@ -8,10 +8,14 @@ public class PalindromaService {
     public String esPalindroma(String palabra) {
         String response;
         String invertida = new StringBuilder(palabra).reverse().toString().toLowerCase();
-        if (invertida.equals(palabra.toLowerCase())) {
-            response = palabra + " es una palabra palíndroma";
-        } else {
+        if (palabra.length() == 1) {
             response = palabra + " no es una palabra palíndroma";
+        } else {
+            if (invertida.equals(palabra.toLowerCase())) {
+                response = palabra + " es una palabra palíndroma";
+            } else {
+                response = palabra + " no es una palabra palíndroma";
+            }
         }
         return response;
     }
